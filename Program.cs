1f70afb90233d6ac93233eb00f8f0c5a7c6778e6
Han Hossain <hanhossain@microsoft.com>
2022-10-04 14:06:00 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace OutputTest
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
